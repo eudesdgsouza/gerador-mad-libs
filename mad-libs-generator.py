853b032e8684494c62335cdf01f1b65a0a4ad599
eudesdgsouza <dev.eudesdgs@gmail.com>
2025-01-06 16:31:22 -0300

    initial commit

---
+++ mad-libs-generator.py
@@ -0,0 +1,31 @@
+# Perguntas para o usuário responder
+
+noun = input('Escolha um substantivo: ')
+
+p_noun = input('Escolha um substantivo plural: ')
+
+noun2 = input('Escolha um substantivo: ')
+
+place = input('Nomeie um lugar: ')
+
+adjective = input('Escolha um adjetivo (palavra descritiva): ')
+
+noun3 = input('Escolha um substantivo: ')
+
+# Imprimir uma história a partir da entrada do usuário
+
+print('------------------------------------------')
+
+print('Seja gentil com o seu', noun, '- com pés', p_noun)
+
+print('Pois um pato pode ser alguém\'s', noun2, ',')
+
+print('Seja gentil com o seu', p_noun, 'em', place)
+
+print('Onde o clima está sempre', adjective, '. \n')
+
+print('Você pode pensar que é isso', noun3, ',')
+
+print('Bem, é.')
+
+print('------------------------------------------')
